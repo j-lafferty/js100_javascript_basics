5ac8ce802f279771666ccec1684f0362d506bbfc
James Lafferty <james@j-lafferty.net>
2020-06-15 15:24:28 -0700

    Add solution for array element comparison

---
+++ array_travel.js
@@ -0,0 +1,17 @@
+//The destinations array contains a list of travel destinations.
+
+let destinations = ['Prague', 'London', 'Sydney', 'Belfast', 'Rome',
+  'Aruba', 'Paris', 'Bora Bora', 'Barcelona', 'Rio de Janeiro',
+  'Marrakesh', 'New York City'];
+/*
+Write a function that checks whether or not a particular destination is included within destinations, without using the built-in method Array.prototype.includes().
+
+For example: When checking whether 'Barcelona' is contained in destinations, the expected output is true, whereas the expected output for 'Nashville' is false.
+*/
+
+const contains = (city, dest) => {
+    return dest.indexOf(city) > 0;
+};
+
+console.log(contains('Barcelona', destinations)); // true
+console.log(contains('Nashville', destinations)); // false
